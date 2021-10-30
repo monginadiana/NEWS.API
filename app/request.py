9d4d83d283287b89c35d58df8fea5e146185d79b
monginadiana <diana.ondieki@student.moringa.com>
2021-10-30 20:33:43 +0300

    getting the api key and the news url

--- app/request.py
+++ app/request.py
@@ -4,6 +4,13 @@
 # api_key = app.config['NEWS_API_KEY']
 from app import app
 import urllib.request,json
+
+from app.source_test import Source
 from .models import source
 
-Movie = source.Source
+Source = source.Source
+# Getting api key
+api_key = app.config['MOVIE_API_KEY']
+
+# Getting the movie base url
+base_url = app.config["MOVIE_API_BASE_URL"]
