a22c02d00f932be59733aa0d9161695da7be0b1c
monginadiana <diana.ondieki@student.moringa.com>
2021-10-30 21:44:38 +0300

    addition of the source description for the new objects to be created in each file

--- app/source_test.py
+++ app/source_test.py
@@ -11,7 +11,7 @@ class SourceTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_source = Source(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+        self.new_source = Source('blasting-news-br','Blasting News (BR)','Descubra a seção brasileira da Blasting News, a primeira revista feita pelo  público, com notícias globais e vídeos independentes. Junte-se a nós e torne- se um repórter.','https://br.blastingnews.com','general','br','pt')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Source))
