21fe633fd18f515542284ebb1f3ab517535ddb9e
monginadiana <diana.ondieki@student.moringa.com>
2021-10-30 20:31:54 +0300

    creating a function in the request.py that gets data from the api

--- app/request.py
+++ app/request.py
@@ -1,4 +1,9 @@
-from app import app
+# from app import app
 
-# Getting api key
-api_key = app.config['NEWS_API_KEY']
+# # Getting api key
+# api_key = app.config['NEWS_API_KEY']
+from app import app
+import urllib.request,json
+from .models import source
+
+Movie = source.Source
