bb5afab703ea3e370fdbb298f0158ac8057ae98f
monginadiana <diana.ondieki@student.moringa.com>
2021-10-30 19:42:21 +0300

    creation of the navbar

--- app/views.py
+++ app/views.py
@@ -1,11 +1,20 @@
 from flask import render_template
 from app import app
 
-# Views
 @app.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    title = 'Home - Welcome to The bestnew article Website Online'
+    return render_template('index.html', title = title)
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html',id = news_id)
